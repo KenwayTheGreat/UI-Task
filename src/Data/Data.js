4a9eef14f7caf94f263818adc5774bc7856a58fe
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-11-24 00:49:26 +0200

    Data Creation:      Created mock data for content.

---
+++ src/Data/Data.js
@@ -0,0 +1,136 @@
+export const menu = [
+  {
+    icon: "dashboard",
+    name: "Student Dashboard",
+  },
+  {
+    icon: "pending",
+    name: "Pending Experiments",
+  },
+  {
+    icon: "assigned",
+    name: "Assigned Experiments",
+  },
+  {
+    icon: "completed",
+    name: "Completed Experiments",
+  },
+  {
+    icon: "unassigned",
+    name: "Unassigned Experiments",
+  },
+  {
+    icon: "trophy",
+    name: "Trophies & Achievements",
+  },
+];
+
+export const experiments = [
+  {
+    type: "Pending Experiments",
+    status: "pending",
+    amount: 9,
+  },
+  {
+    type: "New Experiments",
+    status: "new",
+    amount: 8,
+  },
+  {
+    type: "Unassigned Experiments",
+    status: "unassigned",
+    amount: 9,
+  },
+  {
+    type: "Completed Experiments",
+    status: "completed",
+    amount: 25,
+  },
+];
+
+export const studentAnalysis = [
+  { ability: "Scientific Thinking", amount: "90%" },
+  { ability: "Data Analysis", amount: "80%" },
+  { ability: "Application", amount: "56%" },
+  { ability: "Technical Skills", amount: "95%" },
+];
+
+export const result = {
+  amount: 95,
+  grade: "A+",
+  status: "excellent",
+};
+
+export const trophies = [
+  { name: "football" },
+  { name: "Theatre" },
+  { name: "debate" },
+];
+
+export const pendingExperiments = [
+  {
+    name: "Carbon Dioxide",
+    subject: "Chemistry",
+    requirements: {
+      grade: "29",
+      total: "30",
+    },
+    status:"pending",
+    deadline: "25/11/2021",
+  },
+
+  {
+    name: "Mountain Rushmore Research",
+    subject: "American History",
+    requirements: {
+      grade: "25",
+      total: "30",
+    },
+    status:"pending",
+    deadline: "25/11/2021",
+  },
+
+  {
+    name: "Gravity Research",
+    subject: "Physics",
+    requirements: {
+      grade: "20",
+      total: "30",
+    },
+    status:"pending",
+    deadline: "25/11/2021",
+  },
+
+  {
+    name: "Gravity Research",
+    subject: "Physics",
+    requirements: {
+      grade: "20",
+      total: "30",
+    },
+    status:"pending",
+    deadline: "29/11/2021",
+  },
+
+  {
+    name: "Gravity Research",
+    subject: "Physics",
+    requirements: {
+      grade: "20",
+      total: "30",
+    },
+    status:"new",
+    deadline: "29/11/2021",
+  },
+
+  {
+    name: "Gravity Research",
+    subject: "Physics",
+    requirements: {
+      grade: "20",
+      total: "30",
+    },
+    status:"new",
+    deadline: "29/11/2021",
+  },
+];
