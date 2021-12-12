e6ad147520c9bd69c93d1c0278eb095c794b6dfc
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-12-12 03:30:23 +0200

    Added firebase library and added file configuration.

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2MYmeYzUQTL-53M1SYvm_horOWFc0EPU",
+  authDomain: "school-system-dcdf0.firebaseapp.com",
+  projectId: "school-system-dcdf0",
+  storageBucket: "school-system-dcdf0.appspot.com",
+  messagingSenderId: "823591227306",
+  appId: "1:823591227306:web:7b68e8aabee94b09fd25d3",
+  measurementId: "G-Y0EZF41CDB",
+};
+
+const app = initializeApp(firebaseConfig);
